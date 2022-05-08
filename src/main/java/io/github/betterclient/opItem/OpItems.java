c0f75e987ce610e48e3edf27ab0b9449958eb2ee
betterclient <103489620+betterclient@users.noreply.github.com>
2022-05-08 15:44:13 +0300

    Update and rename src/main/java/com/example/example_mod/ExampleMod.java to src/main/java/io/github/betterclient/opItem/OpItems.java

---
+++ src/main/java/io/github/betterclient/opItem/OpItems.java
@@ -0,0 +1,13 @@
+package io.betterclient.opItem;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpItems implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
