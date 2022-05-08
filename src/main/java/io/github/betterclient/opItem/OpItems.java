fb367297a0a57ee178cdeab79421716a443d5cb5
betterclient <103489620+betterclient@users.noreply.github.com>
2022-05-08 20:14:27 +0300

    Update OpItems.java

--- src/main/java/io/github/betterclient/opItem/OpItems.java
+++ src/main/java/io/github/betterclient/opItem/OpItems.java
@@ -2,12 +2,116 @@ package io.github.betterclient.opItem;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.quiltmc.qsl.item.group.api.QuiltItemGroup;
+import org.quiltmc.qsl.item.group.api.QuiltItemGroup.Builder;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
 
 public class OpItems implements ModInitializer {
 	@Override
 	public void onInitialize(ModContainer mod) {
-
+		Builder builder = QuiltItemGroup.builder(new Identifier("opitems", "optab"));
+		builder.appendItems(t -> getItems());
+		builder.icon(() -> new ItemStack(Blocks.BARRIER));
+		builder.build();
+	}
+	
+	public DefaultedList<ItemStack> getItems(){
+		DefaultedList<ItemStack> stacks = DefaultedList.of();
+		
+		ItemStack sharp32ksword = new ItemStack(Items.NETHERITE_SWORD);
+		sharp32ksword.addEnchantment(Enchantment.byRawId(16), 32767);//Sharpness
+		sharp32ksword.addEnchantment(Enchantment.byRawId(20), 32767);//Fire aspect
+		sharp32ksword.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		sharp32ksword.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(sharp32ksword);
+		
+		ItemStack sharp32kaxe = new ItemStack(Items.NETHERITE_AXE);
+		sharp32kaxe.addEnchantment(Enchantment.byRawId(32), 32767);//Efficiency
+		sharp32kaxe.addEnchantment(Enchantment.byRawId(16), 32767);//Sharpness
+		sharp32kaxe.addEnchantment(Enchantment.byRawId(20), 32767);//Fire aspect
+		sharp32kaxe.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		sharp32kaxe.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(sharp32kaxe);
+		
+		ItemStack eff32kpickaxe = new ItemStack(Items.NETHERITE_PICKAXE);
+		eff32kpickaxe.addEnchantment(Enchantment.byRawId(32), 32767);//Efficiency
+		eff32kpickaxe.addEnchantment(Enchantment.byRawId(35), 10);//Fortune
+		eff32kpickaxe.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		eff32kpickaxe.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(eff32kpickaxe);
+		
+		ItemStack silktouchpickaxe = new ItemStack(Items.NETHERITE_PICKAXE);
+		silktouchpickaxe.addEnchantment(Enchantment.byRawId(32), 32767);//Efficiency
+		silktouchpickaxe.addEnchantment(Enchantment.byRawId(33), 1);//Silk Touch
+		silktouchpickaxe.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		silktouchpickaxe.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(silktouchpickaxe);
+		
+		ItemStack eff32kshovel = new ItemStack(Items.NETHERITE_SHOVEL);
+		eff32kshovel.addEnchantment(Enchantment.byRawId(32), 32767);//Efficiency
+		eff32kshovel.addEnchantment(Enchantment.byRawId(35), 10);//Fortune
+		eff32kshovel.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		eff32kshovel.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(eff32kshovel);
+		
+		ItemStack all32khelmet = new ItemStack(Items.NETHERITE_HELMET);
+		all32khelmet.addEnchantment(Enchantment.byRawId(0), 32767);//Protection
+		all32khelmet.addEnchantment(Enchantment.byRawId(1), 32767);//Fire Protection
+		all32khelmet.addEnchantment(Enchantment.byRawId(3), 32767);//Blast Protection
+		all32khelmet.addEnchantment(Enchantment.byRawId(4), 32767);//Projectile Protection
+		all32khelmet.addEnchantment(Enchantment.byRawId(5), 32767);//Respiration
+		all32khelmet.addEnchantment(Enchantment.byRawId(7), 32767);//Thorns
+		all32khelmet.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		all32khelmet.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(all32khelmet);
+		
+		ItemStack all32kchestplate = new ItemStack(Items.NETHERITE_CHESTPLATE);
+		all32kchestplate.addEnchantment(Enchantment.byRawId(0), 32767);//Protection
+		all32kchestplate.addEnchantment(Enchantment.byRawId(1), 32767);//Fire Protection
+		all32kchestplate.addEnchantment(Enchantment.byRawId(3), 32767);//Blast Protection
+		all32kchestplate.addEnchantment(Enchantment.byRawId(4), 32767);//Projectile Protection
+		all32kchestplate.addEnchantment(Enchantment.byRawId(7), 32767);//Thorns
+		all32kchestplate.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		all32kchestplate.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(all32kchestplate);
+		
+		ItemStack all32kleggings = new ItemStack(Items.NETHERITE_LEGGINGS);
+		all32kleggings.addEnchantment(Enchantment.byRawId(0), 32767);//Protection
+		all32kleggings.addEnchantment(Enchantment.byRawId(1), 32767);//Fire Protection
+		all32kleggings.addEnchantment(Enchantment.byRawId(3), 32767);//Blast Protection
+		all32kleggings.addEnchantment(Enchantment.byRawId(4), 32767);//Projectile Protection
+		all32kleggings.addEnchantment(Enchantment.byRawId(7), 32767);//Thorns
+		all32kleggings.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		all32kleggings.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(all32kleggings);
+		
+		ItemStack all32kboots = new ItemStack(Items.NETHERITE_BOOTS);
+		all32kboots.addEnchantment(Enchantment.byRawId(0), 32767);//Protection
+		all32kboots.addEnchantment(Enchantment.byRawId(1), 32767);//Fire Protection
+		all32kboots.addEnchantment(Enchantment.byRawId(3), 32767);//Blast Protection
+		all32kboots.addEnchantment(Enchantment.byRawId(4), 32767);//Projectile Protection
+		all32kboots.addEnchantment(Enchantment.byRawId(7), 32767);//Thorns
+		all32kboots.addEnchantment(Enchantment.byRawId(2), 32767);//Feather falling
+		all32kboots.addEnchantment(Enchantment.byRawId(70), 32767);//Mending
+		all32kboots.addEnchantment(Enchantment.byRawId(34), 32767);//Unbreaking
+		stacks.add(all32kboots);
+		
+		stacks.add(new ItemStack(Blocks.COMMAND_BLOCK));
+		stacks.add(new ItemStack(Blocks.BARRIER));
+		stacks.add(new ItemStack(Blocks.STRUCTURE_BLOCK));
+		stacks.add(new ItemStack(Blocks.STRUCTURE_VOID));
+		stacks.add(new ItemStack(Blocks.LIGHT));
+		stacks.add(new ItemStack(Items.COMMAND_BLOCK_MINECART));
+		stacks.add(new ItemStack(Blocks.DRAGON_EGG));
+		stacks.add(new ItemStack(Items.DEBUG_STICK));
+		stacks.add(new ItemStack(Blocks.SPAWNER));
+		
+		return stacks;
 	}
 }
